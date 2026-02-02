527908d992585ae874978fb817141994909ab0f8
oteniel-santos <tynelsantos@gmail.com>
2026-02-01 22:57:39 -0400

    add desenvolvido por oteniel santos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,16 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50">
       <CadastroForm />
+      <div className="text-center mt-10 mb-4 text-xs">
+        Desenvolvido por:
+        <a
+          href="https://wa.me/5566992028229"
+          className="font-semibold text-shadow-blue-500 hover:text-indigo-900"
+        >
+          Oteniel Santos
+        </a>
+        - Departamento de Transporte Escolar
+      </div>
     </main>
   );
 }
